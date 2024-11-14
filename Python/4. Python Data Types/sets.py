2aa7a09f2a058a20dca6befd4ff14f794250cf68
uhassantoor <uhassantoor@gmail.com>
2024-11-14 11:10:08 +0500

    Python Data Types

---
+++ Python/4. Python Data Types/sets.py
@@ -0,0 +1,13 @@
+#create a new sets
+
+std_id = {2,22,44,55,12}
+print('Student ids....', std_id)
+
+# mix data
+std = {'Umer',22,'Hassan',20,804}
+print(std)
+
+#empty set
+empty_set = set()
+
+empty = {}
---
+++ Python/4. Python Data Types/tuple.py
@@ -0,0 +1,11 @@
+numbers = [1,3,6,5]
+print(numbers)
+
+#Tuple
+cars = ('BMW','AUDI','HONDA' , 'Nissan' )
+print(cars)
+#Modify
+cars[2] = 'NISSAN'
+print(cars)
+for car in cars:
+    print(car)
