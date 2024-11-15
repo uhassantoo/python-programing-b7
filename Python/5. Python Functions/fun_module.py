e869d3d4bd9ceed78a3828acda426e0653278679
uhassantoor <uhassantoor@gmail.com>
2024-11-15 11:41:20 +0500

    Python Functions

---
+++ Python/5. Python Functions/fun_module.py
@@ -0,0 +1,24 @@
+#python modules
+from math import *
+from statistics import *
+
+print('PI VALUE.. ', pi)
+
+
+print(__name__)
+def main():
+    print('hELLOW')
+    
+if __name__ == "__main__":
+    main()
+    
+__file__
+
+
+# Python Program to Find Numbers Divisible by Another Number
+# Python Program to Convert Decimal to Binary, Octal and Hexadecimal
+# Python Program to Find ASCII Value of Character
+# Python Program to Find the Factors of a Number
+# Python Program to Make a Simple Calculator
+# Python Program to Display Calendar
+# Python Program to Find Factorial of Number Using Recursion
